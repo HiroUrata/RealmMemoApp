bb4fc9f5c8e5c581d360a7c35d0880ce400e95d5
HiroUrata <1712026@i.softbank.jp>
2021-09-05 15:06:34 +0900

    RealmDataModel 作成

---
+++ RealmMemoApp/RealmCRUDGroup.swift
@@ -0,0 +1,9 @@
+//
+//  RealmCRUDGroup.swift
+//  RealmMemoApp
+//
+//  Created by UrataHiroki on 2021/09/05.
+//
+
+import Foundation
+import 
---
+++ RealmMemoApp/RealmDataModel.swift
@@ -0,0 +1,16 @@
+//
+//  RealmDataModel.swift
+//  RealmMemoApp
+//
+//  Created by UrataHiroki on 2021/09/05.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmDataModel:Object{
+    
+    @objc dynamic var realmDataOne = String()
+    @objc dynamic var realmDataTwo = String()
+    
+}
