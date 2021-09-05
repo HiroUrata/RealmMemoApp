da2dfda4031ef77286b42ef502ab687b47b3acf4
HiroUrata <1712026@i.softbank.jp>
2021-09-05 15:46:51 +0900

    RealmCRUDGroup Realmへの書き込み　読み込み　追加

--- RealmMemoApp/RealmCRUDGroup.swift
+++ RealmMemoApp/RealmCRUDGroup.swift
@@ -6,4 +6,52 @@
 //
 
 import Foundation
-import 
+import RealmSwift
+
+class RealmCRUDGroup{
+    
+    static var resultRealmArray:[[String:String]] = []
+    
+}
+
+extension RealmCRUDGroup{
+    
+    static func createRealm(createDataOne:String,createDataTwo:String){
+        
+        do{
+            let realm = try Realm()
+            let realmDataModel = RealmDataModel()
+            realmDataModel.realmDataOne = createDataOne
+            realmDataModel.realmDataTwo = createDataTwo
+            
+            try realm.write({
+                
+                realm.add(realmDataModel)
+            })
+            
+        }catch{
+            
+            
+        }
+        
+    }
+    
+}
+
+extension RealmCRUDGroup{
+    
+    static func readAllRealm(){
+        
+        do{
+            let realm = try Realm()
+            resultRealmArray = []
+            
+            for result in realm.objects(RealmDataModel.self){
+                
+                self.resultRealmArray.append(["OneData":result.realmDataOne,"TwoData":result.realmDataTwo])
+            }
+        }catch{
+            
+        }
+    }
+}
