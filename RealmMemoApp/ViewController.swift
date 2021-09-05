37c566e1ae5bf01a272d332ea4c4992a6a6c5971
HiroUrata <1712026@i.softbank.jp>
2021-09-05 16:05:28 +0900

    RealmのデータをtableViewに反映

--- RealmMemoApp/ViewController.swift
+++ RealmMemoApp/ViewController.swift
@@ -19,6 +19,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         let gradient = CAGradientLayer()
         gradient.frame = CGRect(x: view.frame.minX, y: view.frame.minY, width: view.frame.size.width, height: view.frame.size.height)
         gradient.colors = [UIColor.systemIndigo.cgColor,UIColor.white.cgColor]
@@ -33,5 +36,52 @@ class ViewController: UIViewController {
         DesignGroup.targetDesign(targetView: addView, targetButton: addButtton, targetTableView: tableView)
     }
 
+    @IBAction func add(_ sender: Any) {
+        
+        RealmCRUDGroup.createRealm(createDataOne: upTextField.text!, createDataTwo: underTextField.text!)
+        
+        if RealmCRUDGroup.resultRealmArray.count >= 0{
+
+            RealmCRUDGroup.readAllRealm()
+            tableView.reloadData()
+        }
+        
+    }
 }
 
+
+
+extension ViewController:UITableViewDelegate{
+    
+    
+}
+
+extension ViewController:UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        return tableView.frame.height / 7
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        RealmCRUDGroup.resultRealmArray.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let cellLeftLabel = cell.contentView.viewWithTag(1) as! UILabel
+        let cellRightLabel = cell.contentView.viewWithTag(2) as! UILabel
+        
+        cellLeftLabel.text = RealmCRUDGroup.resultRealmArray[indexPath.row]["OneData"]
+        cellRightLabel.text = RealmCRUDGroup.resultRealmArray[indexPath.row]["TwoData"]
+        
+        return cell
+    }
+    
+    
+    
+}
